144120d8ba2168a1202684dd4705ccdea84c2d51
ajshres <ajay.gopal.shrestha@hotmail.com>
2017-10-05 13:19:54 +0545

    Adding inner folder structure

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'twitter-feedback',
+    podModulePrefix: 'appfiles',
     environment,
     rootURL: '/',
     locationType: 'auto',
