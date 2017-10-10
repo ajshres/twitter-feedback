0107abd45861a21bb93874ea78aed12073336ef5
ajshres <ajay.gopal.shrestha@hotmail.com>
2017-10-10 14:29:01 +0545

    Deleting files

--- app/appfiles/index/route.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-    setupController(controller) {
-        this._super(...arguments);
-        controller.set('recentHash',controller.get('session').getVariable('recentHash',[]));
-        // controller.get('tweetsTask').perform('#weareone');
-    }
-});
--- app/helpers/moment.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function momentjs([date]/*, hash*/) {
-  return moment(date).fromNow();
-}
-
-export default Ember.Helper.helper(momentjs);
